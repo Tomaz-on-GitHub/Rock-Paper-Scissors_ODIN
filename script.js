51f5cbdcaf3b06c573366c62e0689bf8b3ef7fb6
Tomaz_on_git <gumimixer@gmail.com>
2022-10-20 14:04:00 +0200

    create basic functionality of the game (button input)

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 "use strict";
-console.log("Script file connected");
-//there are 5 rounds,
+//console.log("Script file connected");
+
+//start by adding listerens to objects
+const keys = document.querySelectorAll('.key');
+// we use the .forEach method to iterate through each button
+keys.forEach((key) => { key.addEventListener('click', function (e) { playRound(e.target.id)})});
+  
+
+
+//initialize counters
 let player=0; //initial number of points of human player
 let comp=0;
 let round=0; //it is the zero round played
 let currentWin = 0; //here statuso of current play will be saved;
 
-function myGame()  {
+/*
+function myGame1()  {
     for (let i = 0; i < 5; i++) {
         
         // your code here!
@@ -28,26 +37,27 @@ function myGame()  {
         document.getElementById("result1").innerHTML += "points:" + player + " you lose! "
         }
 }
+*/
 //three steps of single round (function playRound)
 //playRound();
 function playRound(playerSelection) {
     // your code here!
     //1. get input from player (person)
-   // let playerSelection = input1();
+    //console.log("player choice: " +  playerSelection );
+    document.getElementById("playerSelection").innerText += playerSelection + ',';
     //2. get input from computer (script)
-    console.log("player choice: " +  playerSelection );
-    document.getElementById("playerSelection").innerHTML += playerSelection + ',';
     let computerSelection = getComputerChoice();
-    console.log("computer choice: " + computerSelection);
-    document.getElementById("computerSelection").innerHTML += computerSelection+ ',';
+    //console.log("computer choice: " + computerSelection);
+    document.getElementById("computerSelection").innerText += computerSelection+ ',';
     //3. compare both inputs (suppose they are valid!), declare winner ,count points
-    if (playerSelection !== 0 && computerSelection !== 0 ) { //valid results only
+    if (playerSelection !== 0 && computerSelection !== 0 ) 
+    { //valid results only
         if (computerSelection === playerSelection) {
             //both are equal, there is no winner
             console.log("same selection");
-            player +=0.5;
-            comp +=0.5;
-            currentWin="even"
+            player +=0;
+            comp +=0;
+            currentWin="even"  //make the color appropriately
         } else if (computerSelection == "paper" && playerSelection =="rock"){
             comp +=1;
             console.log("comp wins: " + comp);
@@ -70,35 +80,12 @@ function playRound(playerSelection) {
     }
     //write the round and current results:
     round++; //we add to the round
+    showRoundWinner(currentWin);
     document.getElementById("round1").innerHTML = "Round: " + round ;
-    document.getElementById("result1").innerHTML = currentWin + "(points:" + player + ")";
+    if (player===5 || comp===5) declareWinner();
+   // document.getElementById("result1").innerHTML = currentWin + "(points:" + player + ")";
 
-    //if this is the 5th round, than declare the winner
-    //declare winner
-    if (round===5) {
-        if (player>comp) {
-            console.log("you win! Good job!")
-            document.getElementById("result1").innerHTML= 
-            "points:" + player + ":" + comp + " you win! Good job! "
-            document.getElementById("result1").style.backgroundColor ="green";
-            }
-        else if (player === comp) {
-            document.getElementById("result1").innerHTML= "points:" + player + " result is even! "
-            console.log("even")
-            document.getElementById("result1").style.backgroundColor ="orange";
-            }
-        else {
-            console.log("you lose!")
-            document.getElementById("result1").innerHTML= "points:" + player + " you lose! "
-            document.getElementById("result1").style.backgroundColor ="red";
-            }
-    }
-    //if this is the 6th round, than this is a new game (reset everything and start new)...
-    if (round===6) {
-        location.reload();
-    }
- //console.log(playRound(playerSelection, computerSelection));
- 
+   
   }
 
 /*
@@ -140,9 +127,53 @@ function myFunction(argument) {
  
  //play five rounds
 
- //declare absolute winner
+ //declare round winner (on page)
+  function showRoundWinner(winner)
+  {
+    //winner possibilites:even, lose, win (perspective of human player)
+    if (winner==="even") {document.getElementById("playerH").style.backgroundColor ="orange";}
+    if (winner==="win") {document.getElementById("playerH").style.backgroundColor ="green";}
+    if (winner==="lose") {document.getElementById("playerH").style.backgroundColor ="red";}
+    if (winner==="even") {document.getElementById("playerC").style.backgroundColor ="orange";}
+    if (winner==="win") {document.getElementById("playerC").style.backgroundColor ="red";}
+    if (winner==="lose") {document.getElementById("playerC").style.backgroundColor ="green";}
+    document.getElementById("resultPlayer").innerText=player;
+    document.getElementById("resultComputer").innerText=comp;
+    //console.log("winner" + winner);
+  };
 
  
+  function declareWinner()  //declare winner
+    {
+        if (player>comp) {
+           // console.log("you win! Good job!")
+            document.getElementById("result1").innerHTML= 
+            " you win! Good job! "
+            document.getElementById("result1").style.backgroundColor ="green";
+            }
+        
+        else {
+           // console.log("you lose!")
+            document.getElementById("result1").innerHTML= " you lose! "
+            document.getElementById("result1").style.backgroundColor ="red";
+            }
+
+            //put all counters to zero
+            player=0; //initial number of points of human player
+            comp=0;
+            round=0; //it is the zero round played
+            currentWin = 0; //here statuso of current play will be saved;
+           // document.getElementById("result1").innerHTML=""
+            document.getElementById("result1").style.backgroundColor ="white";
+          //  document.getElementById("playerSelection").innerText = "";
+          //  document.getElementById("computerSelection").innerText = "";
+   // }
+    //if this is the 6th round, than this is a new game (reset everything and start new)...
+   // if (round===6) {
+   //     location.reload();
+  //  }
+    //console.log(playRound(playerSelection, computerSelection));
+    }
    
  
   
